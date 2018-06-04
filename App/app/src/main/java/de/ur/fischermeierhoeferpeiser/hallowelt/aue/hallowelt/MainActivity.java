47917650ae65403f348a5fa6e8b27efb78664520
Sebastian Peiser <peacecake@web.de>
2018-06-04 17:34:34 +0200

    Implemented login in mainActivity, added register activity which is still empty

--- App/app/src/main/java/de/ur/fischermeierhoeferpeiser/hallowelt/aue/hallowelt/MainActivity.java
+++ App/app/src/main/java/de/ur/fischermeierhoeferpeiser/hallowelt/aue/hallowelt/MainActivity.java
@@ -1,13 +1,63 @@
 package de.ur.fischermeierhoeferpeiser.hallowelt.aue.hallowelt;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import de.ur.fischermeierhoeferpeiser.hallowelt.aue.hallowelt.auth.RegisterActivity;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private EditText etUsername;
+    private EditText etPassword;
+    private Button btnLogin;
+    private TextView tvRegister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        initLayout();
+        initListeners();
     }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btnLogin:
+                loginUser();
+                break;
+            case R.id.tvRegister:
+                registerUser();
+                break;
+        }
+    }
+
+    private void initLayout() {
+        setContentView(R.layout.activity_main);
+        etUsername = (EditText) findViewById(R.id.etUsername);
+        etPassword = (EditText) findViewById(R.id.etPassword);
+        btnLogin = (Button) findViewById(R.id.btnLogin);
+        tvRegister = (TextView) findViewById(R.id.tvRegister);
+    }
+
+    private void initListeners() {
+        btnLogin.setOnClickListener(this);
+        tvRegister.setOnClickListener(this);
+    }
+
+    private void loginUser() {
+        Toast.makeText(this, "Login "+etUsername.getText()+" with password " + etPassword.getText() + "", Toast.LENGTH_SHORT).show();
+    }
+
+    private void registerUser() {
+        Intent i = new Intent(this, RegisterActivity.class);
+        startActivity(i);
+
+    }
+
 }
