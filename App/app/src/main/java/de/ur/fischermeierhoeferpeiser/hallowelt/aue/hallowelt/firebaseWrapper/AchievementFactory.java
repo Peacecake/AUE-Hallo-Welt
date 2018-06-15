cb9180c42f06898609c81b39f6dc45dc6472e95b
Sebastian Peiser <peacecake@web.de>
2018-06-15 20:29:29 +0200

    achievement looks a bit nicer, clean up, achievement for first visited location

--- App/app/src/main/java/de/ur/fischermeierhoeferpeiser/hallowelt/aue/hallowelt/firebaseWrapper/AchievementFactory.java
+++ App/app/src/main/java/de/ur/fischermeierhoeferpeiser/hallowelt/aue/hallowelt/firebaseWrapper/AchievementFactory.java
@@ -6,9 +6,11 @@ public class AchievementFactory {
     private static final int TRAVEL_ACHIEVEMENT_THRESHOLD = 3;
 
     public static Achievement checkTravelAchievement(ArrayList<Location> visitedLocations) {
+        if (visitedLocations.size() == 1) {
+            return new Achievement("Neu hier?", "Du hast die erste Sehenswürdigkeit besucht, willkommen in Regensburg!");
+        }
         if (visitedLocations.size() % TRAVEL_ACHIEVEMENT_THRESHOLD == 0) {
-            Achievement a = new Achievement("Viel-Reiser", "Du hast drei neue Orte besucht, Glückwunsch!");
-            return  a;
+            return new Achievement("Erkunder", "Du hast drei neue Orte besucht, Glückwunsch!");
         }
         return null;
     }
--- App/app/src/main/java/de/ur/fischermeierhoeferpeiser/hallowelt/aue/hallowelt/helpers/AchievementDialog.java
+++ App/app/src/main/java/de/ur/fischermeierhoeferpeiser/hallowelt/aue/hallowelt/helpers/AchievementDialog.java
@@ -1,38 +1,16 @@
 package de.ur.fischermeierhoeferpeiser.hallowelt.aue.hallowelt.helpers;
 
-import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.app.DialogFragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.view.Window;
 import android.widget.TextView;
 
 import de.ur.fischermeierhoeferpeiser.hallowelt.aue.hallowelt.R;
-import de.ur.fischermeierhoeferpeiser.hallowelt.aue.hallowelt.firebaseWrapper.Achievement;
 
 public class AchievementDialog extends DialogFragment implements View.OnClickListener {
-
-    private Activity activity;
-    private Achievement achievement;
-    private TextView tvHeader;
-    private TextView tvDescription;
-    private TextView tvClose;
-
-    /*public AchievementDialog(Activity activity, Achievement achievement) {
-        super(activity);
-        this.activity = activity;
-        this.achievement = achievement;
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        setContentView(R.layout.achievement_dialog);
-        tvHeader = findViewById(R.id.tvAchievementTitle);
-        tvDescription = findViewById(R.id.tvAchievementDescription);
-        tvClose = findViewById(R.id.tvCloseAchievement);
-        tvClose.setOnClickListener(this);
-    }*/
-
     public static AchievementDialog newInstance(String title, String description) {
         AchievementDialog f = new AchievementDialog();
 
@@ -43,12 +21,13 @@ public class AchievementDialog extends DialogFragment implements View.OnClickLis
 
         return f;
     }
+
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         String title = getArguments().getString("title");
         String description = getArguments().getString("description");
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-        builder.setTitle("Erfolg freigeschaltet");
+        builder.setTitle(R.string.achievementUnlocked);
         LayoutInflater inflater = getActivity().getLayoutInflater();
         View content = inflater.inflate(R.layout.achievement_dialog, null);
 
