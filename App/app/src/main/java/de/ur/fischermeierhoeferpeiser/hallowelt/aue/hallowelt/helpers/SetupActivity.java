e1bb3b7c06ae6e301b1d9d622280c912f8bfffd0
Sebastian Peiser <peacecake@web.de>
2018-06-15 16:29:50 +0200

    Added my five locations to strings.xml respectivly setup activity

--- App/app/src/main/java/de/ur/fischermeierhoeferpeiser/hallowelt/aue/hallowelt/helpers/SetupActivity.java
+++ App/app/src/main/java/de/ur/fischermeierhoeferpeiser/hallowelt/aue/hallowelt/helpers/SetupActivity.java
@@ -16,6 +16,11 @@ public class SetupActivity extends HelloWorldActivity implements View.OnClickLis
         add(new Location(49.024540, 12.054830, getString(R.string.westbad), getString(R.string.westbadDescription)));
         add(new Location(49.018618, 12.096435, getString(R.string.neupfarrplatz), getString(R.string.neupfarrplatzDescription)));
         add(new Location(49.021974, 12.097153, getString(R.string.steinerneBrücke), getString(R.string.steinerneBrückeDescription)));
+        add(new Location(49.019488, 12.098254, getString(R.string.stpeter), getString(R.string.stpeterDescription)));
+        add(new Location(49.022921, 12.098124, getString(R.string.jahninsel), getString(R.string.jahninselDescription)));
+        add(new Location(49.026383, 12.088610, getString(R.string.dultplatz), getString(R.string.dultplatzDescription)));
+        add(new Location(49.023495, 12.048264, getString(R.string.donaupark), getString(R.string.donauparkDescription)));
+        add(new Location(49.017866, 12.093099, getString(R.string.sax), getString(R.string.saxDescription)));
     }};
 
     private int count;
